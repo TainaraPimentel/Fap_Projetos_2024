2663bd267a9c296832f7081893564e60ee57b58a
tainaraPimentel <tainara_pimentel@outlook.com.br>
2024-08-24 19:27:29 -0300

    estrutura classe

--- banco.js
+++ banco.js
@@ -1,6 +1,29 @@
 
 //Estrutura do código modularizado
 
+let contas = []
+let referencialConta = 1
+
+class Banco (){
+    this.nome = nome
+    this.numeroConta = numeroConta
+    this.dataNascimento = dataNascimento
+    this.telefone = telefone
+    this.saldo = saldo
+
+    creditar (){
+
+    }
+
+    exibirSaldo (){
+
+    }
+
+    debitar (){
+
+    }
+}
+
 function cadastrarUsuario(){
     
 
